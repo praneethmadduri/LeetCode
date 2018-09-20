3aeb4b2c83bda3ba425cd6adaaa250805df1bed8
DESKTOP-LGQN04L\Praneeth-PC <pmaddur1@binghamton.edu>
2018-09-19 21:50:08 -0400

    first commit

---
+++ LeetCode/LeetCode/ReverseInteger.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/*
+Given a 32-bit signed integer, reverse digits of an integer.
+
+Example 1:
+
+Input: 123
+Output: 321
+Example 2:
+
+Input: -123
+Output: -321
+Example 3:
+
+Input: 120
+Output: 21
+Note:
+Assume we are dealing with an environment which could only store integers within the 32-bit signed integer range: [−231,  231 − 1]. For the purpose of this problem, assume that your function returns 0 when the reversed integer overflows.
+*/
+namespace LeetCode
+{
+    class ReverseInteger
+    {
+        public int Reverse(int x)
+        {
+
+            int rev = 0;
+
+
+            while (x != 0)
+            {
+                int i = x % 10;
+                x = x / 10;
+
+                if ((rev > Int32.MaxValue / 10 || rev == Int32.MaxValue / 10 && i > Int32.MaxValue % 10) || (rev < Int32.MinValue / 10 || rev == Int32.MinValue / 10 && i < Int32.MinValue % 10))
+                {
+                    rev = 0;
+                    break;
+                }
+                else
+                {
+                    rev = rev * 10 + i;
+                }
+            }
+
+            return rev;
+
+        }
+    }
+}
---
+++ LeetCode/LeetCode/TwoSum.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/*Given an array of integers, return indices of the two numbers such that they add up to a specific target.
+ * You may assume that each input would have exactly one solution, and you may not use the same element twice.
+ * Example:
+ * Given nums = [2, 7, 11, 15], target = 9,
+ * Because nums[0] + nums[1] = 2 + 7 = 9,
+ * return [0, 1].
+*/
+namespace LeetCode
+{
+    class TwoSum
+    {
+        public int[] TwoSumMethod(int[] nums, int target)
+        {
+
+            Dictionary<int, int> diff = new Dictionary<int, int>();
+            int[] res = new int[2];
+            for (int i = 0; i < nums.Length; i++)
+            {
+                if (diff.ContainsKey(nums[i]))
+                {
+                    res[0] = diff[nums[i]];
+                    res[1] = i;
+                    break;
+                }
+                else
+                {
+                    diff[target - nums[i]] = i;
+                }
+
+            }
+            return res;
+        }
+    }
+}
